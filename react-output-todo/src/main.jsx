7f095dd996bcdddd91651e31138087c001028e11
tabira2382 <tabira2382@gmail.com>
2024-01-18 14:01:34 +0900

    add:main.jsx

--- react-output-todo/src/main.jsx
+++ react-output-todo/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { TodoPage } from "./pages/todo";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <TodoPage />
   </React.StrictMode>,
 )
